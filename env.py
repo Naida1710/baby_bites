1dd1a8c8a532b8ca0bceec39cb87d5e865d40bbd
Naida Hadzic <naida.popovic1993@gmail.com>
2025-08-25 19:22:02 +0200

    Add SECRET_KEY to env.py

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_bYih3remLC6S@ep-divine-truth-a2j74q89.eu-central-1.aws.neon.tech/tweet_strut_alive_146044")
+os.environ.setdefault("SECRET_KEY", "naidaemil14!")
