934d2e9e3da5d92db0c8b4e3d4b465df264c5aeb
Naida Hadzic <naida.popovic1993@gmail.com>
2025-08-25 14:21:45 +0200

    Change urlspatterns in urls.py in my_project file

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from baby_bites import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('baby-bites/', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
