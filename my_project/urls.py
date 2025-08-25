9ecc562ed337fae0713a967697b0f9336d681c6f
Naida Hadzic <naida.popovic1993@gmail.com>
2025-08-25 23:08:34 +0200

    Add a new file in the baby_bites directory, urls.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from baby_bites import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("", include("baby_bites.urls"), name="baby_bites-urls"),
     path('admin/', admin.site.urls),
 ]
