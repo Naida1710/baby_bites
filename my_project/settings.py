e5405da5e16c49d8607359a3c6fc704187ca223c
Naida Hadzic <naida.popovic1993@gmail.com>
2025-08-25 17:25:38 +0200

    Add psycopg2-binary for PostgreSQL support

--- my_project/settings.py
+++ my_project/settings.py
@@ -84,6 +84,9 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 #    }
 # }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
