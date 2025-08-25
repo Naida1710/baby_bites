04eaffb78a51b5bcbfcc4ba2fc42be2aea60a448
Naida Hadzic <naida.popovic1993@gmail.com>
2025-08-25 19:50:12 +0200

    Import the Post model to baby_bites/admin.py

--- baby_bites/models.py
+++ baby_bites/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-status = models.IntegerField(choices=STATUS, default=0)
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
