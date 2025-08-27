f6e21dd662b6ac619db201b9d5e6b1cff3fe089f
Naida Hadzic <naida.popovic1993@gmail.com>
2025-08-27 20:12:56 +0200

    Set SITE_ID, LOGIN_REDIRECT_URL, and LOGOUT_REDIRECT_URL in settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -49,6 +49,11 @@ INSTALLED_APPS = [
     'django_summernote'
 ]
 
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
