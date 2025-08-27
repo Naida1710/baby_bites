6eb00eeb47cb46a537aea11b6b8a3b1415163b4a
Naida Hadzic <naida.popovic1993@gmail.com>
2025-08-27 20:07:54 +0200

    Install and wire up Allauth

--- baby_bites/views.py
+++ baby_bites/views.py
@@ -10,13 +10,14 @@ class EventList(generic.ListView):
     paginate_by = 12
 
 
-def event_detail(request, event_id):
-    
-    queryset = Event.objects.all()
-    event = get_object_or_404(queryset, event_id=event_id)
+def post_detail(request, slug):
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
 
     return render(
         request,
-        "events/event_detail.html",
-        {"event": event}
+        "blog/post_detail.html",
+        {"post": post,
+         "coder": "Matt Rudge"},
     )
--- my_project/settings.py
+++ my_project/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'baby_bites',
     'django_summernote'
 ]
