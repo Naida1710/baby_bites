42d35ae27851ffea36f7ed6b494850f0f34fae01
Naida Hadzic <naida.popovic1993@gmail.com>
2025-09-17 22:03:32 +0200

    Add Post handling for Collaborate form with success message

--- baby_bites/views.py
+++ baby_bites/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.contrib.auth.models import User
 from .models import Post
+from django.contrib import messages
+from .models import About
 from .forms import CommentForm
 from .forms import CollaborateForm
 
 
-
 def about_me(request):
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+            
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
