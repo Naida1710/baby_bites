d47e16556605a5b1b823958565181af6d99d4d7d
Naida Hadzic <naida.popovic1993@gmail.com>
2025-09-17 22:20:20 +0200

    Add URL pattern for editing comments via comment_edit view

--- baby_bites/urls.py
+++ baby_bites/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('recipes/10-months/', views.recipes_10_months, name='recipes_10_months'),
     path('recipes/12-months/', views.recipes_12_months, name='recipes_12_months'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
