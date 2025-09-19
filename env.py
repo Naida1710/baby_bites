f96ddc3186c4727905a82c5e2e8688644aa4dadd
Naida Hadzic <naida.popovic1993@gmail.com>
2025-09-19 12:13:41 +0200

    Update Cloudinary in env.py

--- env.py
+++ env.py
@@ -4,5 +4,4 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_bYih3remLC6S@ep-divine-truth-a2j74q89.eu-central-1.aws.neon.tech/tweet_strut_alive_146044")
 os.environ.setdefault("SECRET_KEY", "naidaemil14!")
 os.environ.setdefault(
-    "CLOUDINARY_URL", "CLOUDINARY_URL=cloudinary://<	
-578431366652723>:<578431366652723>@dcjuuwedl")
+    "CLOUDINARY_URL=cloudinary://<578431366652723>:<SqmULkeCs-o_Xg-5sE0b1T4eXtM>@dcjuuwedl")
