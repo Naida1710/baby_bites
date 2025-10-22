394bf1a7a2fb7f47c73f38a01186f1a17ee060f8
Naida Hadzic <naida.popovic1993@gmail.com>
2025-10-22 08:25:42 +0200

    Use environment variables for secrets

--- env.py
+++ env.py
@@ -1,7 +1,5 @@
 import os
 
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_bYih3remLC6S@ep-divine-truth-a2j74q89.eu-central-1.aws.neon.tech/tweet_strut_alive_146044")
-os.environ.setdefault("SECRET_KEY", "naidaemil14!")
-os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary:/371226436496774:8ExY-4sT3Fz15D3bNhG4XHSibeU")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
