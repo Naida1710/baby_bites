7ef73fd53b47109010a6aa0d065583405b858e9a
Naida Hadzic <naida.popovic1993@gmail.com>
2025-10-22 07:07:11 +0200

    Heroku deployment

--- env.py
+++ env.py
@@ -4,4 +4,4 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_bYih3remLC6S@ep-divine-truth-a2j74q89.eu-central-1.aws.neon.tech/tweet_strut_alive_146044")
 os.environ.setdefault("SECRET_KEY", "naidaemil14!")
 os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://578431366652723:SqmULkeCs-o_Xg-5sE0b1T4eXtM@dcjuuwedl")
+    "CLOUDINARY_URL", "cloudinary:/371226436496774:8ExY-4sT3Fz15D3bNhG4XHSibeU")
